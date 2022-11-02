21dc1440ba19994da51c469b8990665b7e0715e9
Krier Maxime <maxime@pharmanity.com>
2022-11-02 11:10:30 +0100

    fix: change login behavior

--- config.dev.js
+++ config.dev.js
@@ -2,17 +2,17 @@ const crypto = require('crypto');
 
 const cfg = {
 	port: 9090,
-	https: true,
+	https: false,
 	get baseUrl() {
-		return 'https://front.santeclick.staging-pharmanity.fr:' + this.port;
+		return 'http://localhost:' + this.port;
 	},
 	get client() {
 		return {
 			clientId: '000000',
 			clientSecret: '999999',
-			accessTokenUri: 'https://santeclick.staging-pharmanity.fr/token',
-			authorizationUri: 'https://santeclick.staging-pharmanity.fr/authorize',
-			redirectUri: this.baseUrl + this.paths.authFlowCallback,
+			accessTokenUri: 'http://192.168.49.2:3009/token', // url santeclick go
+			authorizationUri: 'http://localhost:8081/authorize', // url santeclick go
+			redirectUri: this.baseUrl + this.paths.authFlowCallback, // url expo ou client callback
 			scopes: ['read'],
 			state: crypto.randomBytes(10).toString('hex'),
 		};
--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const util = require('util');
 const fs = require('fs');
+var cors = require('cors');
 const ClientOAuth2 = require('client-oauth2');
-
+const cfg = require('./config.dev');
 const app = express();
-const cfg = require('./config');
+var corsOptions = {
+	origin: cfg.baseUrl,
+	optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 const client = new ClientOAuth2(cfg.client);
 
 const key = fs.readFileSync('./key.pem');
 const cert = fs.readFileSync('./cert.pem');
-const server = https.createServer({ key: key, cert: cert }, app);
-
+let server = http.createServer(app);
+if (cfg.https) {
+	server = https.createServer({ key: key, cert: cert }, app);
+}
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
@@ -22,11 +32,13 @@ app.get('/', function (req, res) {
 
 app.get(cfg.paths.authFlowStart, function (req, res) {
 	var uri = client.code.getUri();
+	console.log(uri);
 	res.redirect(uri);
 });
 
 app.get(cfg.paths.authFlowCallback, function (req, res) {
 	console.log(cfg.paths.authFlowCallback, req.originalUrl);
+
 	client.code
 		.getToken(req.originalUrl)
 		.then(function (user) {
